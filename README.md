db39870f531aeb99594105107b777d4d5706e326
kazuto1011 <k_nakashima@irvs.ait.kyushu-u.ac.jp>
2017-05-20 16:29:58 +0900

    Update a script and readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Grad-CAM with PyTorch
 
-PyTorch implementation of [Grad-CAM (Gradient-weighted Class Activation Mapping) [1]](https://arxiv.org/pdf/1610.02391v1.pdf)
+PyTorch implementation of [Grad-CAM (Gradient-weighted Class Activation Mapping) [1]](https://arxiv.org/pdf/1610.02391v1.pdf). Grad-CAM localizes and highlights the discriminative regions that Convolutional Neural Networks-based models activate to predict visual concepts. This code contains only an imeplementation for the VGG-19 image classification models.
+
+ - [x] Grad-CAM
+ - [ ] Guided backpropagation and Guided Grad-CAM
 
 ## Dependencies
 * Python 2.7
@@ -8,13 +11,13 @@ PyTorch implementation of [Grad-CAM (Gradient-weighted Class Activation Mapping)
 
 ## Usage
 ```bash
-$ python grad_cam.py [image]
+$ python generate.py [image]
 ```
 
 ## Examples
 |Input|Cauliflower|Cucumber|Broccoli|Head cabbage|Zucchini|
 |:-:|:-:|:-:|:-:|:-:|:-:|
-|![](samples/vegetables.jpg)|![](results/grad_cam_cauliflower.png)|![](results/grad_cam_cucumber.png)|![](results/grad_cam_broccoli.png)|![](results/grad_cam_head_cabbage.png)|![](results/grad_cam_zucchini.png)|
+|![](samples/vegetables.jpg)|![](samples/vegetables/cauliflower.png)|![](samples/vegetables/cucumber.png)|![](samples/vegetables/broccoli.png)|![](samples/vegetables/head_cabbage.png)|![](samples/vegetables/zucchini.png)|
 
 ## References
-[1] R. R. Selvaraju _et al._ "Grad-CAM: Visual Explanations from Deep Networks via Gradient-based Localization". arXiv, 2016
+\[1\] R. R. Selvaraju, A. Das, R. Vedantam, M. Cogswell, D. Parikh, and D. Batra. "Grad-CAM: Visual Explanations from Deep Networks via Gradient-based Localization". arXiv, 2016
--- generate.py
+++ generate.py
@@ -5,6 +5,7 @@
 # URL:      http://kazuto1011.github.io
 # Created:  2017-05-18
 
+from __future__ import print_function
 
 import sys
 from collections import OrderedDict
@@ -13,10 +14,9 @@ import cv2
 import numpy as np
 import torch
 import torch.nn as nn
+import torchvision
 from torch.autograd import Variable
 from torch.nn import functional as F
-
-import torchvision
 from torchvision import transforms
 
 cuda = True
@@ -34,33 +34,39 @@ class GradCAM(object):
         self.outputs_backward = OrderedDict()
         self._set_hook_func()
 
-    def _func_f(self, m, i, o):
-        self.outputs_forward[id(m)] = o.data.cpu()
-
-    def _func_b(self, m, i, o):
-        self.outputs_backward[id(m)] = o[0].cpu()
-
     def _set_hook_func(self):
+
+        def _func_f(m, i, o):
+            self.outputs_forward[id(m)] = o.data.cpu()
+
+        def _func_b(m, i, o):
+            self.outputs_backward[id(m)] = o[0].cpu()
+
         for module in self.model.named_modules():
-            module[1].register_forward_hook(self._func_f)
-            module[1].register_backward_hook(self._func_b)
+            module[1].register_forward_hook(_func_f)
+            module[1].register_backward_hook(_func_b)
 
     def _one_hot(self, idx):
-        one_hot = torch.FloatTensor(1, 1000)
-        one_hot.zero_()
+        one_hot = torch.FloatTensor(1, 1000).zero_()
         one_hot[0][idx] = 1.0
         return one_hot
 
-    def _normalize(self, gradients):
-        l2_norm = torch.sqrt(torch.mean(torch.pow(gradients, 2))) + 1e-5
-        return gradients / l2_norm.data[0]
+    def _normalize(self, grads):
+        l2_norm = torch.sqrt(torch.mean(torch.pow(grads, 2))) + 1e-5
+        return grads / l2_norm.data[0]
 
     def _retrieve_conv_outputs(self, outputs, target_layer):
         for key, value in outputs.items():
-            for module in model.named_modules():
+            for module in self.model.named_modules():
                 if id(module[1]) == key:
                     if module[0] == target_layer:
                         return value
+        raise ValueError('invalid layer name: {}'.format(target_layer))
+
+    def _compute_gradient_weights(self):
+        self.grads = self._normalize(self.grads)
+        self.map_size = self.grads.size()[2:]
+        self.weights = nn.AvgPool2d(self.map_size)(self.grads)
 
     def load_image(self, filename, transform):
         self.image = cv2.imread(sys.argv[1])[:, :, ::-1]
@@ -81,20 +87,18 @@ class GradCAM(object):
             classwise = classwise.cuda()
         self.output.backward(classwise, retain_variables=True)
 
-        self.featuremaps = self._retrieve_conv_outputs(
+        self.fmaps = self._retrieve_conv_outputs(
             self.outputs_forward, self.target_layer)
-        self.gradients = self._retrieve_conv_outputs(
+        self.grads = self._retrieve_conv_outputs(
             self.outputs_backward, self.target_layer)
 
-    def compute_gradient_weights(self):
-        self.gradients = self._normalize(self.gradients)
-        self.map_size = self.gradients.size()[2:]
-        self.weights = nn.AvgPool2d(self.map_size)(self.gradients)
+    def generate_grad_cam(self):
+        self._compute_gradient_weights()
 
-    def computing_cam(self):
         grad_cam = torch.FloatTensor(self.map_size).zero_()
-        for fmap, weight in zip(self.featuremaps[0], self.weights[0]):
+        for fmap, weight in zip(self.fmaps[0], self.weights[0]):
             grad_cam += fmap * weight.data[0][0]
+
         return F.relu(Variable(grad_cam))
 
     def save(self, filename, grad_cam):
@@ -122,21 +126,24 @@ classes = tuple(classes)
 
 
 if __name__ == '__main__':
+
+    print('Loading a model...')
     model = torchvision.models.vgg19(pretrained=True)
     target_layer = 'features.36'
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
-                             0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
     ])
 
+    print('Generating Grad-CAM...')
     cam = GradCAM(model, target_layer)
     cam.load_image(sys.argv[1], transform)
     cam.forward()
+    
     for i in range(0, 5):
-        print '{:.5f}\t{}'.format(cam.prob[i], classes[cam.idx[i]])
+        print('{:.5f}\t{}'.format(cam.prob[i], classes[cam.idx[i]]))
         cam.backward(cam.idx[i])
-        cam.compute_gradient_weights()
-        grad_cam = cam.computing_cam()
-        cam.save(
-            'results/grad_cam_{}.png'.format(classes[cam.idx[i]]), grad_cam)
+        grad_cam = cam.generate_grad_cam()
+        cam.save('results/grad_cam_{}.png'.format(classes[cam.idx[i]]),
+                 grad_cam)
